5d0f178ebe738a18f4da45fc47390f37c2481256
DavidCornejoArias <davidcornejoarias@gmail.com>
2023-08-05 18:23:32 -0400

    Update runningScript_loop.R
    Adding census loop variables

--- runningScript_loop.R
+++ runningScript_loop.R
@@ -7,10 +7,20 @@ library(srvyr)
 library(readxl)
 library(parallel)
 options(scipen = 999)
+# select between "censos"/"encuestas"
+tipo <- "censos"
+# select between country or ine01 for surveys and country or geolev1 for census
+geoLevel <- "geolev1"
+if (tipo=="encuestas"){
 
 available_years <- read.csv("Inputs/running_survey.csv") %>% 
-  filter(availability==1)
+  filter(availability==1) 
+}
 
+if (tipo=="censos"){
+  available_years <- read.csv("Inputs/running_census.csv") %>% 
+    filter(availability==1&person=="David")
+}
 # if needed you can run by chunks of countries here
 #"ARG",   "CHL", "BLZ", "BRA", "COL", "CRI", "ECU", "GUY", "HTI",  "PAN", "PRY", "SUR", "URY"
 
@@ -26,7 +36,7 @@ for (i in 1:nrow(unique_combinations)) {
   # Get country and year from the current row
   pais <- unique_combinations[[i, "Pais"]]
   anio <- unique_combinations[[i, "year"]]
-  tipo <- "encuestas"
+
   
   tryCatch({
     source("scl_indicators.R")
@@ -36,9 +46,9 @@ for (i in 1:nrow(unique_combinations)) {
     }
     
     # Add more conditions for other types if needed
-    #if (tipo == "censos") {
-      # do something
-    #}
+    if (tipo == "censos") {
+      write.csv(data_total, paste("Outputs/indicadores_censos_hogares_", pais, "_", anio, ".csv", sep = ""), row.names = FALSE)
+    }
     
     # Add more code here if needed
     
